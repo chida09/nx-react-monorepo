3e8e68ee95b14340ebf7dccecbde0d2e87184026
takaaki chida <takaaki.chida@findy.co.jp>
2024-07-28 15:05:16 +0900

    add libs to shared

--- apps/first-dir/src/app/app.tsx
+++ apps/first-dir/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
+import { SharedUi } from '@nx-react-monorepo/shared-ui'
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <SharedUi/>
       <NxWelcome title="first-dir" />
     </div>
   );
